308f673f73507ab6626a6ec94681e967f5ce7061
root <root@guxiaolin-VirtualBox>
2016-07-16 11:20:45 +0800

    TWO_AP WORK

--- CSI_process/Location.m
+++ CSI_process/Location.m
@@ -1,14 +1,12 @@
 function [LocationMap, LocationPure] =  Location(datapath)
     LocationPure = csvread(datapath);
-    [row, col] = size(LocationPure)
-        keyset = [LocationPure(1,1)];
-        keyvalue = struct('x',LocationPure(1,2),'y',LocationPure(1,3));
-        LocationMap = containers.Map(keyset, keyvalue);
-    if(row <= 1)
+    LocationMap = [] ;
+    row = size(LocationPure,1);
+    if(row < 1)
         return 
     end
-    for i = 2:row
-        keyset = [i];
+    for i = 1:row
+        keyset = i;
         keyvalue = struct('x',LocationPure(i,2),'y',LocationPure(i,3));
         loc_temp = containers.Map(keyset, keyvalue);
         LocationMap = [LocationMap;loc_temp];
